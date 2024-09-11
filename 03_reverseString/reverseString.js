9ac7a993df46a8172b03efb6a639a7ef7c429e86
CRISP <crisp@crisperience.com>
2024-09-11 12:03:39 +0200

    Solved the 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = reverseString;
+const reverseString = function (string) {
+    let reversedArray = string.split('');
+    reversedArray.reverse();
+    return reversedArray.join('');
+  };
+  
+  // Do not edit below this line
+  module.exports = reverseString;
+  
